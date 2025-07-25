4cee4975c1e83e73f1dcaae120c18af287822da4
Ronival Santos <ronivalsantos16@gmail.com>
2025-07-25 15:34:43 -0300

    alteracoes info profile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,27 @@
-(async function showJson() {
+(async function () {
   const profileData = await fetchProfileData();
-  console.log(profileData);
+  uptadeProfileData(profileData);
 })();
+
+function uptadeProfileData(profileData) {
+  const profileImg = document.querySelector('.profile-photo');
+  profileImg.src = profileData.photo;
+
+  const nome = document.querySelector('.profile-name');
+  profileImg.alt = profileData.nome;
+  nome.innerText = profileData.nome;
+
+  const job = document.querySelector('.profile-job');
+  job.innerText = profileData.job;
+
+  const location = document.querySelector('.profile-location');
+  location.innerText = profileData.localidade.cidade;
+
+  const phone = document.querySelector('.profile-phone');
+  phone.innerText = profileData.telefone;
+  phone.href = `tel:${profileData.telefone}`;
+
+  const email = document.querySelector('.profile-email');
+  email.innerText = profileData.email;
+  email.href = `mailto:${profileData.email}`;
+}
